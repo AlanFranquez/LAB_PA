407b70c24db0b4606bb7c405b5435c56ffd20099
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-08-13 22:12:34 -0300

    Update Sistema.java

--- src/serverCentral/Sistema.java
+++ src/serverCentral/Sistema.java
@@ -1,5 +1,18 @@
 package serverCentral;
 
-public class Sistema {
+public class Sistema implements ISistema {
+    private static Sistema instance = null;
 
+    private Sistema() {
+        //  colecciones
+    }
+
+    public static synchronized Sistema getInstance() {
+        if (instance == null) {
+            instance = new Sistema();
+        }
+        return instance;
+    }
+
+    //DESTRUCTOR¿?
 }
