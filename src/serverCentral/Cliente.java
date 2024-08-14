1f5109a79ad170573f8a8170456bbe630dc13f01
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-08-14 13:03:35 -0300

    Update Cliente.java

--- src/serverCentral/Cliente.java
+++ src/serverCentral/Cliente.java
@@ -1,5 +1,28 @@
 package serverCentral;
 
-public class Cliente {
+public class Cliente extends Usuario {
+    private List<OrdenDeCompra> listaCompras;
+    private List<Comentario> listaComentarios;
+    // Constructor
+    public Cliente(String Nom, String Ni, String Ape, String Cor, Date Naci) {
+        super(Nom, Ni, Ape, Cor, Naci); // constructor de Usuario
+        this.listaCompras = new ArrayList<>();
+        this.listaComentarios = new ArrayList<>();
+    }
+    // gets, sets
+    public List<OrdenDeCompra> getCompras() {
+        return listaCompras;
+    }
 
+    public List<Comentario> getComentarios() {
+        return listaComentarios;
+    }
+    //opers
+    public Set<Integer> getAllOrdenes() {
+        Set<Integer> RES = new HashSet<>();
+        for (OrdenDeCompra ordenActual : listaCompras) {
+            RES.add(ordenActual.getId());
+        }
+        return RES;
+    }
 }
