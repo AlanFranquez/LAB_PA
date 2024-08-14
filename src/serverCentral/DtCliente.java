5551d9ba50cefc695cd9f4ed005e642d38987689
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-08-14 18:51:42 -0300

    arreglado DtCliente.java

--- src/serverCentral/DtCliente.java
+++ src/serverCentral/DtCliente.java
@@ -2,14 +2,16 @@ public class dtCliente {
     private String nombre, nick, apellido, correo;
     private Date nacimiento;
     private File imagen;
-
-    public dtCliente(String nombre, String nick, String apellido, String correo, Date nacimiento, File imagen) {
+    private Map<Integer, OrdenDeCompra> ordenes;
+    
+    public dtCliente(String nombre, String nick, String apellido, String correo, Date nacimiento, File imagen, Map<Integer, OrdenDeCompra> ordenes) {
         this.nombre = nombre;
         this.nick = nick;
         this.apellido = apellido;
         this.correo = correo;
         this.nacimiento = nacimiento;
         this.imagen = imagen;
+        this.ordenes = ordenes;
     }
     // Gets
     public String getNombre() {
@@ -29,5 +31,8 @@ public class dtCliente {
     }
     public File getImagen() {
         return imagen;
+    }
+    public Map<Integer, OrdenDeCompra> getOrdenes() {
+        return ordenes;
     }
 }
