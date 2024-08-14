2fc6daaa27e6bd4a9102655f0d2c3cd58a192a2f
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-08-14 13:17:31 -0300

    Update Proveedor.java

--- src/serverCentral/Proveedor.java
+++ src/serverCentral/Proveedor.java
@@ -1,5 +1,34 @@
 package serverCentral;
 
-public class Proveedor {
+public class Proveedor extends Usuario {
+    private List<Producto> listaProductos;  
+    private String compania, link;
+    // Constructr
+    public Proveedor(String nom, String ni, String ape, String cor, Date naci, String comp, String lin) {
+        super(nom, ni, ape, cor, naci);
+        this.compania = comp;
+        this.link = lin;
+        this.listaProductos = new ArrayList<>();
+    }
+    // Gets sets
+    public String getCompania() {
+        return compania;
+    }
 
-}
+    public void setCompania(String comp) {
+        this.compania = comp;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String lin) {
+        this.link = lin;
+    }
+
+    public List<OrdenDeCompra> getProductos() {
+        return listaProductos;
+    }
+
+  
