425af281e62495ae806ed72e4f3a06636db61cbf
Fabricio Fuentes <fuentesfabricio17@gmail.com>
2024-08-20 20:52:14 -0300

    Comentario

--- src/serverCentral/Comentario.java
+++ src/serverCentral/Comentario.java
@@ -1,5 +1,26 @@
 package serverCentral;
 
+import java.time.LocalDateTime;
+
 public class Comentario {
+	private String contenido;
+	private LocalDateTime fecha;
+	
+	public Comentario (String cont) {
+		fecha = LocalDateTime.now();
+		contenido = cont;
+	}
+
+	public String getContenido() {
+		return contenido;
+	}
+
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
 
 }
